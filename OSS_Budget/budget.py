a998a8d29aa94bcfc4f5b211a98b4331a1e8367a
KimBukyeong <taejune1501@naver.com>
2025-06-10 11:15:09 +0900

    Update budget.py.김부경.영남대교육학과.22320787

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -25,3 +25,24 @@ class Budget:
         print(f"총 지출: {total}원\n")
 
 
+    def __init__(self):
+        self.imports=[]
+
+    def add_expense(self, category, description, amount):
+        today = datetime.date.today().isoformat()
+        expense = Imports(today, category, description, amount)
+        self.imports.append(imports)
+        print("지출이 추가되었습니다.\n")
+
+    def list_expenses(self):
+        if not self.imports:
+            print("수입 내역이 없습니다.\n")
+            return
+        print("\n[수입 목록]")
+        for idx, e in enumerate(self.imports, 1):
+            print(f"{idx}. {e}")
+        print()
+
+    def total_spent(self):
+        total = sum(e.amount for e in self.imports)
+        print(f"총 수입: {total}원\n")
